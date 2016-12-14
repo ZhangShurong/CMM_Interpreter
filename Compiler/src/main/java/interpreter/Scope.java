0c2e97c6c8f6a0ee4e79f3c73a1fefac0be5a7a7
Alfred_Zhang <Zhang_Shurong@foxmail.com>
2016-12-14 17:30:17 +0800

    修改Scope

--- Compiler/src/main/java/interpreter/Scope.java
+++ Compiler/src/main/java/interpreter/Scope.java
@@ -1,22 +1,76 @@
 package interpreter;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Created by vergil on 2016/12/7.
  */
-public interface Scope {
+public class Scope {
+    /*
 
-    String getScopeName();
+    Scope enclosingScope; // null if global (outermost) scope
+    Map<String, Symbol> symbols = new LinkedHashMap<String, Symbol>();
+
+    public BaseScope(Scope enclosingScope) { this.enclosingScope = enclosingScope;  }
+
+    public Symbol resolve(String name) {
+        Symbol s = symbols.get(name);
+        if ( s!=null ) return s;
+        // if not here, check any enclosing scope
+        if ( enclosingScope != null ) return enclosingScope.resolve(name);
+        return null; // not found
+    }
+
+    public void define(Symbol sym) {
+        symbols.put(sym.name, sym);
+        sym.scope = this; // track the scope in each symbol
+    }
+
+    public boolean redundant(String name){
+        return symbols.get(name) != null;
+    }
+
+    public Scope getEnclosingScope() { return enclosingScope; }
+
+    public String toString() { return getScopeName()+":"+symbols.values().toString(); }
+     */
+
+    private Scope enclosingScope;
+    private Map<String, Symbol> symbols = new LinkedHashMap<String, Symbol>();
+
+    public Scope(Scope enclosingScope)
+    {
+        this.enclosingScope =enclosingScope;
+    }
+
+    String getScopeName()
+    {
+        return null;
+    }
 
     /** Where to look next for symbols */
-    Scope getEnclosingScope();
+    Scope getEnclosingScope()
+    {
+        return null;
+    }
 
     /** Define a symbol in the current scope */
-    void define(Symbol sym);
+    void define(Symbol sym)
+    {
+
+    }
 
     /** Determine redundant definition in same scope */
-    boolean redundant(String name);
+    boolean redundant(String name)
+    {
+        return false;
+    }
 
     /** Look up name in this scope or in enclosing scope if not here */
-    Symbol resolve(String name);
+    Symbol resolve(String name)
+    {
+        return null;
+    }
 
 }
