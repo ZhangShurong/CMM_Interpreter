cc9a1c0cfd220ffa4954058a2778af78ac521077
kelei <keleiwhu@163.com>
2016-12-18 19:36:49 +0800

    add .toString()

--- Compiler/src/main/java/io/ConsoleIO.java
+++ Compiler/src/main/java/io/ConsoleIO.java
@@ -12,13 +12,13 @@ public class ConsoleIO implements IOInterface {
 
     @Override
     public void stdout(Object out) {
-        System.out.println((String) out);
+        System.out.println((String) out.toString());
     }
 
     @Override
     public void stderr(Object out) {
         System.out.println("Error\n--------------------------------");
-        System.out.println((String) out);
+        System.out.println((String) out.toString());
         System.out.println("-------------------------------------");
     }
 }
